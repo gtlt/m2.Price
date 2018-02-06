89f03bf7ffa4cb2b52fde70be882b9ac496e6ba6
karliuka <karliuka.vitalii@gmail.com>
2018-02-06 23:07:47 +0200

    blocks

--- Block/Adminhtml/Form/Field/Import.php
+++ Block/Adminhtml/Form/Field/Import.php
@@ -23,35 +23,4 @@ class Import extends AbstractElement
         parent::_construct();
         $this->setType('file');
     }
-
-    /**
-     * Enter description here...
-     *
-     * @return string
-     */
-    public function getElementHtml()
-    {
-        $html = '';
-
-        $html .= '<input id="time_condition" type="hidden" name="' . $this->getName() . '" value="' . time() . '" />';
-
-        $html .= <<<EndHTML
-        <script>
-        //require(['prototype'], function(){
-        //Event.observe($('carriers_tablerate_condition_name'), 'change', checkConditionName.bind(this));
-        //function checkConditionName(event)
-        //{
-        //    var conditionNameElement = Event.element(event);
-        //    if (conditionNameElement && conditionNameElement.id) {
-        //        $('time_condition').value = '_' + conditionNameElement.value + '/' + Math.random();
-        //    }
-        //}
-        //});
-        </script>
-EndHTML;
-
-        $html .= parent::getElementHtml();
-
-        return $html;
-    }
 }
